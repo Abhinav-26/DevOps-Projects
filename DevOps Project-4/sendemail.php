0653d7634760f85519ad55d0c95c323dc34f91d7
ongeziwe njokweni <37049195+ongie2@users.noreply.github.com>
2022-07-31 01:20:17 +0200

    changes

--- DevOps Project-4/sendemail.php
+++ DevOps Project-4/sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'trendytheme.net@gmail.com';  // put your email here
+    $to         = 'ongenjokwein@gmail.com';  // put your email here
 
     $email_template = 'simple.html';
 
